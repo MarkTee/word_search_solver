b3d879087cd24d366559c7075bf4e546fac9a1c7
Mark Thomas <MarkTee@users.noreply.github.com>
2018-08-17 01:16:21 -0600

    Remove terminal node functionality
    For our purposes, such an implementation is unnecessary

--- trie.py
+++ trie.py
@@ -1,13 +1,12 @@
 class Trie:
     """A very basic implentation of a trie
 
-    Supports adding words, determining whether a given prefix exists within the
-    Trie, and determining whether a string is one of the trie's terminal nodes.
+    Supports adding words and determining whether a given prefix exists within
+    the Trie.
     """
 
     def __init__(self):
         self.head = TrieNode()
-        self.terminal_nodes = []
 
     def insert_word(self, word):
         """Adds a word (and all of the prefixes that it contains) to the trie."""
@@ -25,11 +24,7 @@ class Trie:
         # Add nodes for any prefixes that aren't already present in the trie
         if remaining_prefixes is not None:
             for letter in word[remaining_prefixes:]:
-                # Denote the last letter of the word as a terminal node
-                if index == len(word):
-                    node.add_child(letter, True)
-                else:
-                    node.add_child(letter)
+                node.add_child(letter)
                 node = node.children[letter]
                 index += 1
 
@@ -55,19 +50,12 @@ class TrieNode:
     parent node's dictionaries.
     """
 
-    def __init__(self, terminal_node=False):
+    def __init__(self):
         self._children = {}
-        self.terminal_node = terminal_node
 
-    def add_child(self, child, terminal_node=False):
+    def add_child(self, child):
         """Adds a descendant (which shares a common prefix) to the given node."""
-        if terminal_node:
-            self._children[child] = TrieNode(terminal_node=True)
-        else:
-            self._children[child] = TrieNode()
-
-    def is_terminal_node(self):
-        return self.terminal_node
+        self._children[child] = TrieNode()
 
     @property
     def children(self):

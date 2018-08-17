78c2a173681f1265dabf26b94eef62be8c391bc3
Mark Thomas <MarkTee@users.noreply.github.com>
2018-08-17 01:15:13 -0600

    Clean up trie implementation

--- test_trie.py
+++ test_trie.py
@@ -3,10 +3,10 @@ from trie import *
 def test_find_prefix():
     trie = Trie()
     trie.insert_word('dog')
-    assert trie.find_prefix('a') == False
-    assert trie.find_prefix('d') == True
-    assert trie.find_prefix('o') == False
-    assert trie.find_prefix('do') == True
-    assert trie.find_prefix('g') == False
-    assert trie.find_prefix('dogg') == False
+    assert trie.contains_prefix('a') == False
+    assert trie.contains_prefix('d') == True
+    assert trie.contains_prefix('o') == False
+    assert trie.contains_prefix('do') == True
+    assert trie.contains_prefix('g') == False
+    assert trie.contains_prefix('dogg') == False
 
--- trie.py
+++ trie.py
@@ -1,43 +1,46 @@
 class Trie:
-    """A very basic implentation of a trie.
+    """A very basic implentation of a trie
 
-    Only supports adding words and determining whether a given prefix exists
-    within it.
+    Supports adding words, determining whether a given prefix exists within the
+    Trie, and determining whether a string is one of the trie's terminal nodes.
     """
 
     def __init__(self):
-        self._head = TrieNode()
+        self.head = TrieNode()
+        self.terminal_nodes = []
 
     def insert_word(self, word):
-        """Adds a word to the trie (as well as all intermediate nodes)."""
-        node = self._head
-        remaining_letters_index = None
+        """Adds a word (and all of the prefixes that it contains) to the trie."""
+        node = self.head
+        remaining_prefixes = None
 
+        # Traverse the tree until a prefix that isn't contained in the trie is found
         for index, letter in enumerate(word):
             if letter in node.children:
                 node = node.children[letter]
             else:
-                remaining_letters_index = index
+                remaining_prefixes = index
                 break
 
-        # Add nodes for any letters that aren't already present in the trie
-        if remaining_letters_index is not None:
-            for letter in word[index:]:
-                index += 1
+        # Add nodes for any prefixes that aren't already present in the trie
+        if remaining_prefixes is not None:
+            for letter in word[remaining_prefixes:]:
+                # Denote the last letter of the word as a terminal node
                 if index == len(word):
                     node.add_child(letter, True)
                 else:
                     node.add_child(letter)
                 node = node.children[letter]
+                index += 1
 
-    def find_prefix(self, prefix):
+    def contains_prefix(self, prefix):
         """Determines whether a given prefix exists in the trie."""
-        node = self._head
+        current_node = self.head
         prefix_in_trie = True
 
         for letter in prefix:
-            if letter in node.children:
-                node = node.children[letter]
+            if letter in current_node.children:
+                current_node = current_node.children[letter]
             else:
                 prefix_in_trie = False
                 break
@@ -46,18 +49,25 @@ class Trie:
 
 
 class TrieNode:
-    """Represents the nodes of a trie. Nodes don't contain data."""
+    """Represents the nodes of a trie
+
+    Nodes don't contain data as they're referred to by the keys in their
+    parent node's dictionaries.
+    """
 
     def __init__(self, terminal_node=False):
         self._children = {}
-        self._terminal_node = terminal_node
+        self.terminal_node = terminal_node
 
-    def add_child(self, child, is_terminal_node=False):
-        """Adds a descendant (which will have a common prefix) to the node."""
-        if is_terminal_node:
-            self._children[child] = TrieNode(True)
+    def add_child(self, child, terminal_node=False):
+        """Adds a descendant (which shares a common prefix) to the given node."""
+        if terminal_node:
+            self._children[child] = TrieNode(terminal_node=True)
         else:
             self._children[child] = TrieNode()
+
+    def is_terminal_node(self):
+        return self.terminal_node
 
     @property
     def children(self):
